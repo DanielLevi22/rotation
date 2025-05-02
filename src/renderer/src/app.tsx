e43a93b2808c3c2a932323608ff83a0161bac78b
Daniel Levi <daniellevi000@gmail.com>
2025-05-02 15:09:53 -0300

    Initialize Electron application with React and TypeScript setup. Added configuration files (.eslintrc.json, .gitignore, electron-builder.yml, package.json, tsconfig.json) and basic project structure including main, preload, and renderer directories. Included essential dependencies and development scripts. Added initial styles and assets for the application.

---
+++ src/renderer/src/app.tsx
@@ -0,0 +1,11 @@
+export function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-blue-950">ALo
+          <p className="text-3xl text-blue-950"> 
+              
+          </p>
+      </h1>
+    </>
+  )
+}
